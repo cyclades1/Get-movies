2a134534ac454cfc43683971401d13598c879930
Abhishek Kumar Dubey <abhishek.dubey@geeksforgeeks.org>
2022-01-16 17:18:54 +0530

    removed files

--- main.js
+++
@@ -1,6 +0,0 @@
-function send()
-{
-	var msg = document.getElementById("msg").value;
-    document.getElementById("message-area").innerHTML += msg+" :You<br>";
-    document.getElementById("msg").value = ''
-}
